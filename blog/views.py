798d4e7faac182bbca0ee4e7d72ff92b90236610
Archibald-prog <archpod@gmail.com>
2023-10-16 05:15:14 +0300

    Edited posts_list view, added post_detail view.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 
+from .models import Post
+
 
 def posts_list(request):
-    name_lst = ['John', 'Nick', 'Mary', 'Margaret']
+    posts = Post.objects.all()
     context = {
-        'names': name_lst
+        'posts': posts
     }
     return render(request, 'blog/index.html', context)
+
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    context = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context)
