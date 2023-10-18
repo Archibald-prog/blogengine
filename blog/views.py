45fa0960c6aea9b12c9a237c53b691184f75c765
Archibald-prog <archpod@gmail.com>
2023-10-18 16:24:25 +0300

    Added tags_list and tag_detail views.

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from .models import Post
+from .models import Post, Tag
 
 
 def posts_list(request):
@@ -17,3 +17,19 @@ def post_detail(request, slug):
         'post': post
     }
     return render(request, 'blog/post_detail.html', context)
+
+
+def tags_list(request):
+    tags = Tag.objects.all()
+    context = {
+        'tags': tags
+    }
+    return render(request, 'blog/tags_list.html', context)
+
+
+def tag_detail(request, slug):
+    tag = Tag.objects.get(slug__iexact=slug)
+    context = {
+        'tag': tag
+    }
+    return render(request, 'blog/tag_detail.html', context)
