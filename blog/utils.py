43fcacef7ae1e9679fbaa91236c3f8f2bf4b8806
Archibald-prog <archpod@gmail.com>
2023-10-25 04:42:07 +0300

    Added ObjectUpdateMixin.

--- blog/utils.py
+++ blog/utils.py
@@ -47,6 +47,33 @@ class ObjectCreateMixin:
             return redirect(new_obj)
         return render(request, self.template, context)
 
+
+class ObjectUpdateMixin:
+    model = None
+    model_form = None
+    template = None
+
+    def get(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(instance=obj)
+        context = {
+            'form': bound_form,
+            self.model.__name__.lower(): obj
+        }
+        return render(request, self.template, context)
+
+    def post(self, request, slug):
+        obj = self.model.objects.get(slug__iexact=slug)
+        bound_form = self.model_form(request.POST, instance=obj)
+        context = {
+            'form': bound_form,
+            self.model.__name__.lower(): obj
+        }
+        if bound_form.is_valid():
+            new_obj = bound_form.save()
+            return redirect(new_obj)
+        return render(request, self.template, context)
+
 # class ObjectListMixin:
 #     model = None
 #     template = None
