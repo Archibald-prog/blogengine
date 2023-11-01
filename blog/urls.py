25a2c4ea84d86d6d31622e14a29f6728328acac0
Archibald-prog <archpod@gmail.com>
2023-11-01 05:18:07 +0300

    Edited 'posts_list_url' and 'tags_list_url'.

--- blog/urls.py
+++ blog/urls.py
@@ -3,12 +3,12 @@ from django.urls import path
 from blog.views import *
 
 urlpatterns = [
-    path('', posts_list, name='posts_list_url'),
+    path('', PostsList.as_view(), name='posts_list_url'),
     path('post/create/', PostCreate.as_view(), name='post_create_url'),
     path('post/<str:slug>/', PostDetail.as_view(), name='post_detail_url'),
     path('post/<str:slug>/update/', PostUpdate.as_view(), name='post_update_url'),
     path('post/<str:slug>/delete/', PostDelete.as_view(), name='post_delete_url'),
-    path('tags/', tags_list, name='tags_list_url'),
+    path('tags/', TagsList.as_view(), name='tags_list_url'),
     path('tags/create/', TagCreate.as_view(), name='tag_create_url'),
     path('tags/<str:slug>/', TagDetail.as_view(), name='tag_detail_url'),
     path('tags/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
